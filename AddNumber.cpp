036758e6d325a45620a78a9dcb999fd41c14cc7f
Bushra Khan <151651183+student-Bushrakhan@users.noreply.github.com>
2024-10-23 23:54:10 +0500

    Create AddNumber.cpp

---
+++ AddNumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// Write C++ program to add two integer values and print the output with descriptive 
+//message.
+int main(){
+	int num1;
+	int num2;
+	cout<<"Enter a number1:";
+	cin>>num1;
+	cout<<"Enter a number2:";
+	cin>>num2;
+	int result = num1 + num2;
+    cout << "The sum of the two numbers is: " << result << endl;
+    return 0;
+	
+}
